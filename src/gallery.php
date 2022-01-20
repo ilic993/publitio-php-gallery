13fe1a962e721d8405d64d4473405f2c87eb01dd
ilic993 <34837063+ilic993@users.noreply.github.com>
2022-01-20 20:35:13 +0100

    Adding pagination to our gallery

--- src/gallery.php
+++ src/gallery.php
@@ -5,13 +5,19 @@ namespace PhpGallery;
 require 'vendor/autoload.php';
 require_once('src/class.variables.php');
 
+$page = isset($_GET['page']) ? $_GET['page'] : 1;
+$offset = ($page - 1) * Variables::$PER_PAGE;
+
 $publitio = new \Publitio\API(Variables::$API_KEY, Variables::$API_SECRET);
 $images = $publitio->call('/files/list', 'GET', [
+    'offset' => $offset, 
     'limit' => Variables::$PER_PAGE, 
     'folder' => Variables::$FOLDER, 
     'order' => 'date:desc',
     'filter_type' => 'image'
 ]);
+
+$pages = ceil($images->files_total / Variables::$PER_PAGE) ?? 1;
 ?>
 
 <div class="gallery">
@@ -23,4 +29,16 @@ $images = $publitio->call('/files/list', 'GET', [
         </div>
 
     <?php endforeach; ?>
+
+    <div class="pagination">
+
+        <?php if($page != 1): ?>
+            <a href="?page=<?php echo $page-1; ?>" class="back"></a>
+        <?php endif; ?>
+
+        <?php if($page != $pages): ?>
+            <a href="?page=<?php echo $page+1; ?>" class="forward"></a>
+        <?php endif; ?>
+
+    </div>
 </div>
