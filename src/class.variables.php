cac8335b7c5c319e66e8096a3802e39b0a5821b9
ilic993 <34837063+ilic993@users.noreply.github.com>
2022-01-20 20:50:49 +0100

    Reverting variables file, adding image

---
+++ src/class.variables.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace PhpGallery;
+
+class Variables{
+
+    // API key and secret got from https://publit.io/dashboard/api
+    public static $API_KEY = "xxx";
+    public static $API_SECRET = "yyy";
+
+    // Publitio folder made in advance for this project
+    public static $FOLDER = "zzz";
+
+    // Images displayed per page in gallery
+    public static $PER_PAGE = 5;
+}
